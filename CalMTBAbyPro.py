79603c8d4471f7ef5b21f221af4c7808d01f0008
Kiki Lin <mikey2862@gmail.com>
2019-12-19 13:43:26 +0800

    add timesleep and solve encoding problem

--- CalMTBAbyPro.py
+++ CalMTBAbyPro.py
@@ -1,5 +1,6 @@
 import re
 from datetime import datetime
+import time
 
 logs = []
 newlogs = []
@@ -20,7 +21,7 @@ filename = input('please input the file name(ex.LG07_0520.txt): ' )
 #input_err_number = input('please input the error number you want to check or enter to check all.(ex: 40, for K0040  切痕検査:太寛): ')
 #pro_id = input('please input the device name(ex.LG06-SN100-D-ASE-325-90-G-01):
 #with open(filename,'r',encoding='latin1') as f:
-with open(filename,'r') as f:
+with open(filename,'r', encoding='latin1') as f:
     for line in f:
         if line.find('PRO') != -1 or line.find('ERRSET') != -1 or line.find('FAS')!= -1 or line.find('FAE')!= -1:
         #if line.find('PRO') != -1 or line.find('ERRSET') != -1:
@@ -30,7 +31,8 @@ with open(filename,'r') as f:
 #        print(line)
 newlogs = logs[::-1]
 
-with open('checkLOG.csv', 'w',encoding = 'utf-8') as fcheck:
+#with open('checkLOG.csv', 'w',encoding = 'utf-8') as fcheck:
+with open('checkLOG.csv', 'w', encoding='latin1') as fcheck:
     for line in newlogs:
         fcheck.write(str(line) + '\n')
 #for line in newlogs:
@@ -41,8 +43,8 @@ for newline in newlogs:
     #error_number = newline[2]
     #error_comment = newline[3] #same as pro_id ex:'2019/05/21 23:10:56', 'ERRSET', '39', 'K0039  切痕検査:偏離中心.'
     if newline[1] == 'FAS':
-        print('\n' + 'fullcut start at ' + newline[0])
-        fullcutReco.append('\n' + 'fullcut start at ' + newline[0])
+        #print('\n' + 'fullcut start at ' + newline[0])
+        fullcutReco.append('\n' + 'fullcut start time: ' + newline[0])
         pro_cnt = 0
         all_err_cnt = 0
         err_cnt = 0
@@ -61,7 +63,7 @@ for newline in newlogs:
     elif newline[1] == 'ERRSET': #對應所有error code
         all_err_cnt = all_err_cnt + 1
         errtime = newline[0]
-        print(newline[3] + ' occurred at ' + str(errtime) + ' '+ str(all_err_cnt) + ' times')
+        #print(newline[3] + ' occurred at ' + str(errtime) + ' '+ str(all_err_cnt) + ' times')
         fullcutReco.append(newline[3] + ' occurred at ' + str(errtime) + ' '+ str(all_err_cnt) + ' times')
     # elif newline[1] == 'ERRSET' and newline[2] == input_err_number:
     #     err_cnt = err_cnt + 1
@@ -71,10 +73,10 @@ for newline in newlogs:
     elif newline[1] == 'FAE':
         fae_time = datetime.strptime(newline[0],'%Y/%m/%d %H:%M:%S')
         fullcut_time = fae_time - fas_time
-        print('fullcut end at ' + newline[0])
+        #print('fullcut end time: ' + newline[0])
         fullcutReco.append('fullcut end at ' + newline[0])
-        print('自動加工所花費的時間為: ' + str(fullcut_time) + '即為 '+ str(fullcut_time.seconds) + '秒')
-        fullcutReco.append('自動加工所花費的時間為: ' + str(fullcut_time) + '即為 '+ str(fullcut_time.seconds) + '秒')
+        #print('自動加工所花費的時間為: ' + str(fullcut_time) + '即為 '+ str(fullcut_time.seconds) + '秒')
+        fullcutReco.append('total full cut time: ' + str(fullcut_time) + ', '+ str(fullcut_time.seconds) + 'Seconds')
         print('MTBA: ' + str(all_err_cnt/(fullcut_time.seconds)))
         fullcutReco.append('MTBA: ' + str(all_err_cnt/(fullcut_time.seconds)))
 
@@ -82,6 +84,9 @@ for newline in newlogs:
         #     print('MTBA: ' + str(err_cnt/(fullcut_time.seconds)))
         # else:
         #     print('MTBA: ' + str(all_err_cnt/(fullcut_time.seconds)))
-with open('outputLOG.csv', 'w', encoding = 'utf-8') as fout:
+#with open('outputLOG.csv', 'w', encoding = 'utf-8') as fout:
+with open('outputLOG.csv', 'w', encoding='latin1') as fout:
     for line in fullcutReco:
         fout.write(line + '\n')
+print('The file is output. Please check "checkLOG.csv" to get MTBA result.')
+time.sleep(5)
